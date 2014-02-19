1a19ee6721d9677251a91b4f03bdcad289a5c4ed
Bruno Capella <capella.bruno@gmail.com>
2014-02-18 21:52:25 -0300

    adicionando a camada de mapeamento do Nhibernate e SpringNet

---
+++ GetEmpresa.GestorFotografico.Mapeador/fake.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GetEmpresa.GestorFotografico.Mapeador
+{
+    public class fake
+    {
+    }
+}
