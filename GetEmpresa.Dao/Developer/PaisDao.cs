8bcbb1c38e393faddd37a05642745436315ec32a
Bruno Capella <capella.bruno@gmail.com>
2014-03-12 15:24:12 -0300

    Create PaisDao.cs

---
+++ GetEmpresa.Dao/Developer/PaisDao.cs
@@ -0,0 +1,11 @@
+using System;
+using GetEmpresa.GestorFotografo.Domain;
+using GetEmpresa.Dao.Interface;
+
+namespace GetEmpresa.Dao.Developer{
+  public class PaisDao
+        :GenericDao<Pais> , IPaisDao
+  {
+  
+  }
+}
